07162fd219d9464573763f2a4ebb8ad07aace5fe
Aryan Jain <138187618+aryanjainsinghai@users.noreply.github.com>
2024-06-02 04:15:56 +0000

    Variables in js

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,15 @@
+//Variable declaration
+
+const accountId = 13345 // Value of variable is constant
+let accountEmail = "xyz.mail.com"
+var accountPassword = "xxx111" /* 
+                                this is old method of declaring variable
+                                 prefer not to use because of block scope and functional scope
+                                 problem
+                                */
+accountCity = "gwalior"  // this is also not prefer to use
+
+
+console.table([accountId, accountEmail, accountPassword, accountCity])
+
+
