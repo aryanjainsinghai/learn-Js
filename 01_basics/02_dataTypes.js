8354c31e57adce02dabfbc5f91dc2e9afa40c8a2
Aryan Jain <138187618+aryanjainsinghai@users.noreply.github.com>
2024-06-02 09:26:29 +0000

    datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,15 @@
+"use strict"; // Treat all js code as newer version
+
+// alert(3 + 3) // this alert syntax is not valid for nodejs environment, 
+                   //it is only valid for browser 
+
+
+// Data types
+// Numbers/Integers
+//bigint
+//string
+//boolean
+//null
+//undefined
+//symbol
+
